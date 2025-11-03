7c67679cc841d7163782f2fe577cff78e0b93778
homecom <shinyjin921@gmail.com>
2025-11-03 19:23:22 +0900

    sdsdsd

--- javascript/data.js
+++ javascript/data.js
@@ -11,29 +11,29 @@
 // true  true   true
 
 let a=20;
-b= a++;
-console.log( b ); 
-console.log ( a );
+b= ++a;
+console.log("B값 :"+  b ); 
+console.log ("A값 :"+  a );
 
-b = --a;
-console.log( b ); 
-console.log ( a );
+// b = --a;
+// console.log( b ); 
+// console.log ( a );
 
-a=a++;
-console.log( a ); 
+// a=a++;
+// console.log( a ); 
 
 // let isLogin = true;
 // isLogin && console.log( "로그인을 했습니다" );
 
-let isLogin = false;
-isLogin && console.log( "로그인을 했습니다" );
+// let isLogin = false;
+// isLogin && console.log( "로그인을 했습니다" );
 
-let isEmail = false;
-isEmail || console.log( "이메일입력!" );
+// let isEmail = false;
+// isEmail || console.log( "이메일입력!" );
 
-let userName = ""; 
-// 0,nullm,'',false,undefined
-let displayName = userName || "Guest";
-console.log(displayName);
+// let userName = ""; 
+// // 0,nullm,'',false,undefined
+// let displayName = userName || "Guest";
+// console.log(displayName);
 
 
