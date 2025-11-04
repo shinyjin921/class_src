c6eb2117f8905c8967fb82cf6cbee1a1b32a7686
homecom <shinyjin921@gmail.com>
2025-11-04 20:23:12 +0900

    dfdfd

--- javascript/for-2.js
+++ javascript/for-2.js
@@ -2,7 +2,7 @@
  * 배열과 같이 사용되는 for
  * for-of문
  */
-let colors = ['red','green','blue'];
+// let colors = ['red','green','blue'];
 /**
  * color-1:red    0 colors[0]
  * color-2:green  1 colors[1]
@@ -13,12 +13,31 @@ let colors = ['red','green','blue'];
 //   console.log(`color-${i+1} : ${colors[i]}`)
 // }
 
-for(let color of colors){
-  console.log(`색상:${color}`);
-}
+// for(let color of colors){
+//   console.log(`색상:${color}`);
+// }
 
 //for-of 연습문제
 /**배열[34,67,23,12,54] 가장 작은수와 가장 큰 수를 출력해보세요. */
+let a = [34,67,23,12,54,578,13,75,54,78,17,25];
+let low=0;
+let high=0;
+for(let b of a){
+  if(low > b || low == 0){ low = b };
+  if(high < b){ high = b };
+}
+console.log( low );
+console.log( high );
+
+low = 0;
+high = 0;
+
+for( let i=0; i<=a.length-1; i++){
+  if(low > a[i] || low == 0){ low = a[i] };
+  if(high < a[i] ){ high = a[i] };
+}
+console.log( low );
+console.log( high );
 
 /**
  * 객체:모든 사물
@@ -47,4 +66,4 @@ for(let color of colors){
  * 1~25까지 출력
  */
 
-let a=1;
+// let a=1;
